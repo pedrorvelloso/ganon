34f45af53a2ae4b89b0ea1aa03ddfbea356aa1b8
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-08-14 17:49:01 -0300

    🔧 fix: disable react docgen for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
   stories: ['../packages/**/stories/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  typescript: {
+    reactDocgen: false,
+  },
 }

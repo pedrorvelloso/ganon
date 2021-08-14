51f225b47d68307b1dfef9f6ac925549c9d775a6
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-08-14 17:53:52 -0300

    ✨ feat(button): update how to get theme colors

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -1,61 +1,92 @@
-import { darken } from 'polished'
 import {
-  padding,
   margin,
   display,
-  PaddingProps,
   MarginProps,
   DisplayProps,
+  borderRadius,
+  BorderRadiusProps,
 } from 'styled-system'
+import { darken } from 'polished'
 
-import styled from '@ganon/styled'
+import styled, { css } from '@ganon/styled'
+import { ButtonsVariant, DefaultGanonTheme } from '@ganon/theme'
+import { getColor } from '@ganon/theme-utils'
 
 export interface ButtonVariants {
-  variant?: 'primary' | 'secondary' | 'danger' | 'warning' | 'success'
+  variant?: ButtonsVariant
 }
 
-export type ButtonProps = PaddingProps &
-  MarginProps &
+export type TextTransformUppercase = {
+  transformTextUppercase?: boolean
+}
+
+export type ButtonProps = MarginProps &
   DisplayProps &
-  ButtonVariants
+  ButtonVariants &
+  BorderRadiusProps &
+  TextTransformUppercase
+
+const colors = ({ theme, variant }: ButtonVariants & DefaultGanonTheme) => {
+  const v = variant ?? 'primary'
+
+  return css`
+    color: ${getColor({
+      theme,
+      color: theme.buttons[v].textColor,
+    })};
+
+    background-color: ${darken(
+      0.41,
+      getColor({
+        theme,
+        color: theme.buttons[v].backgroundColor,
+      }),
+    )};
+
+    border: 1px solid
+      ${getColor({
+        theme,
+        color: theme.buttons[v].borderColor,
+      })};
+
+    :hover:enabled {
+      background-color: ${getColor({
+        theme,
+        color: theme.buttons[v].hover.backgroundColor,
+      })};
+      color: ${getColor({
+        theme,
+        color: theme.buttons[v].hover.color,
+      })};
+    }
+  `
+}
 
 export const Button = styled.button<ButtonProps>`
-  background-color: ${({ theme, variant }) =>
-    darken(0.41, theme.colors[variant ?? 'primary'])};
-  border: 1px solid
-    ${({ theme, variant }) => theme.colors[variant ?? 'primary']};
+  ${colors}
   border-radius: 5px;
-
-  color: ${({ theme }) => theme.colors.white};
 
   flex: 1;
 
-  text-transform: uppercase;
-  font-family: monospace;
+  font-weight: bold;
+
+  padding: 16px 26px;
 
   transition: all 150ms ease-in-out;
   cursor: pointer;
-
-  padding: 16px 16px;
-
-  :hover:enabled {
-    background-color: ${({ theme, variant }) =>
-      theme.colors[variant ?? 'primary']};
-    color: ${({ theme, variant }) =>
-      darken(0.41, theme.colors[variant ?? 'primary'])};
-  }
+  text-transform: ${({ transformTextUppercase }) =>
+    transformTextUppercase ? 'uppercase' : 'none'};
 
   :disabled {
     cursor: not-allowed;
-    filter: grayscale(0.9);
+    filter: opacity(0.5);
   }
 
-  ${padding}
   ${margin}
   ${display}
+  ${borderRadius}
 `
 
 Button.defaultProps = {
   display: 'flex',
-  variant: 'primary',
 }
--- packages/button/stories/button.stories.tsx
+++ packages/button/stories/button.stories.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
-import { Button } from '..'
+import { Button } from '../src'
 
 export default {
   title: 'Button',
   component: Button,
 }
 
-export const button = () => <Button>Button</Button>
+export const basicUsage = () => (
+  <>
+    <Button transformTextUppercase>Button</Button>
+    <Button variant="secondary" mt="4">
+      Button
+    </Button>
+    <Button variant="success" mt="4">
+      Button
+    </Button>
+    <Button variant="warning" mt="4">
+      Button
+    </Button>
+    <Button variant="danger" mt="4">
+      Button
+    </Button>
+  </>
+)
+
+export const disabled = () => (
+  <>
+    <Button transformTextUppercase disabled>
+      Button
+    </Button>
+    <Button variant="secondary" disabled mt="4">
+      Button
+    </Button>
+    <Button variant="success" disabled mt="4">
+      Button
+    </Button>
+    <Button variant="warning" disabled mt="4">
+      Button
+    </Button>
+    <Button variant="danger" disabled mt="4">
+      Button
+    </Button>
+  </>
+)

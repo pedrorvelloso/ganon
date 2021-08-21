8d6993d19615ddcbdda223d242b18c05b1af83a3
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-08-20 23:16:18 -0300

    ✨ chore(core): add theme prop to GanonProvider

--- packages/core/src/GanonProvider.tsx
+++ packages/core/src/GanonProvider.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { ThemeProvider, GlobalStyles } from '@ganon/styled'
+import { Theme, theme as defaultTheme } from '@ganon/theme'
 
-export const GanonProvider: React.FC = ({ children }) => (
-  <ThemeProvider>
+interface GanonProviderProps {
+  theme?: Theme
+}
+
+export const GanonProvider: React.FC<GanonProviderProps> = ({
+  children,
+  theme = defaultTheme,
+}) => (
+  <ThemeProvider theme={theme}>
     {children}
     <GlobalStyles />
   </ThemeProvider>

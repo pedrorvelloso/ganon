5bc8eb8ba5edb5e19c6acf06b5422012ff2857d9
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-08-20 23:15:32 -0300

    ➕ chore(core): add theme-utils to dependency tree

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,5 +2,6 @@ export * from '@ganon/button'
 export * from '@ganon/styled'
 export * from '@ganon/theme'
 export * from '@ganon/spinner'
+export * from '@ganon/theme-utils'
 
 export * from './GanonProvider'

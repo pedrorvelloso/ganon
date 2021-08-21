beaa3d43252e90da123f297eae6586243b1e6f64
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-08-21 16:29:11 -0300

    💄 chore(components): fix button stories with Stack

--- packages/components/stories/button.stories.tsx
+++ packages/components/stories/button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FiTwitch, FiMessageCircle } from 'react-icons/fi'
 
-import { Stack } from '@ganon/layout'
+import { VStack } from '@ganon/layout'
 
 import { Button } from '../src'
 
@@ -11,18 +11,18 @@ export default {
 }
 
 export const basicUsage = () => (
-  <>
+  <VStack>
     <Button>Button</Button>
     <Button colorScheme="primary">Button</Button>
     <Button colorScheme="secondary">Button</Button>
     <Button colorScheme="success">Button</Button>
     <Button colorScheme="warning">Button</Button>
     <Button colorScheme="danger">Button</Button>
-  </>
+  </VStack>
 )
 
 export const sizes = () => (
-  <Stack direction="column">
+  <VStack>
     <Button size="sm">Button</Button>
     <Button colorScheme="secondary" size="md">
       Button
@@ -30,11 +30,11 @@ export const sizes = () => (
     <Button colorScheme="success" size="lg">
       Button
     </Button>
-  </Stack>
+  </VStack>
 )
 
 export const disabled = () => (
-  <>
+  <VStack>
     <Button disabled>Button</Button>
     <Button colorScheme="secondary" disabled>
       Button
@@ -48,23 +48,23 @@ export const disabled = () => (
     <Button colorScheme="danger" disabled>
       Button
     </Button>
-  </>
+  </VStack>
 )
 
 export const withIcon = () => (
-  <>
+  <VStack>
     <Button rightIcon={<FiTwitch size={20} />}>Button</Button>
     <Button leftIcon={<FiMessageCircle />} colorScheme="secondary">
       Button
     </Button>
-  </>
+  </VStack>
 )
 
 export const loading = () => <Button isLoading>Button</Button>
 
 export const variants = () => (
-  <>
+  <VStack>
     <Button variant="outline">Button</Button>
     <Button variant="ghost">Button</Button>
-  </>
+  </VStack>
 )

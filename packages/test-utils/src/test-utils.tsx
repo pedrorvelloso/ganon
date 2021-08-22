0341f4c678d4fbe183fa11c6e550db7d7e54f761
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-08-22 18:45:47 -0300

    🔨 chore(test-utils): add a11y test function

--- packages/test-utils/src/test-utils.tsx
+++ packages/test-utils/src/test-utils.tsx
@@ -1,17 +1,35 @@
 import React, { ReactElement } from 'react'
 
-import { render, RenderOptions } from '@testing-library/react'
+import { render as _render, RenderOptions } from '@testing-library/react'
+import { axe, toHaveNoViolations, JestAxeConfigureOptions } from 'jest-axe'
+
 import { ThemeProvider } from '@ganon/core'
 import { theme } from '@ganon/theme'
+
+expect.extend(toHaveNoViolations)
 
 const Providers: React.FC = ({ children }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>
 }
 
-const customRender = (
+type RenderFunctionOptions = Omit<RenderOptions, 'wrapper'>
+
+export const render = (ui: ReactElement, options?: RenderFunctionOptions) =>
+  _render(ui, { wrapper: Providers, ...options })
+
+type A11yOptions = RenderFunctionOptions & {
+  axeOptions?: JestAxeConfigureOptions
+}
+
+export const a11y = async (
   ui: ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>,
-) => render(ui, { wrapper: Providers, ...options })
+  { axeOptions, ...options }: A11yOptions = {},
+) => {
+  const componentContainer = render(ui, options).container
+
+  const assertA11y = await axe(componentContainer, axeOptions)
+
+  expect(assertA11y).toHaveNoViolations()
+}
 
 export * from '@testing-library/react'
-export { customRender as render }

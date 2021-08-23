2bbf639a68e592e064dc700b8acb2231565b4f90
Pedro Reis Velloso dos Santos <pedro_reis14@hotmail.com>
2021-08-23 08:50:53 -0300

    ➕ chore(styled): add csstype to increment colors type

--- packages/styled/src/System.type.ts
+++ packages/styled/src/System.type.ts
@@ -1,8 +1,9 @@
 import { ColorsType } from '@ganon/theme'
+import * as CSS from 'csstype'
 
 export interface ColorProps {
-  color?: ColorsType
-  textColor?: ColorsType
-  bg?: ColorsType
-  backgroundColor?: ColorsType
+  color?: ColorsType | CSS.Property.Color
+  textColor?: ColorsType | CSS.Property.Color
+  bg?: ColorsType | CSS.Property.Color
+  backgroundColor?: ColorsType | CSS.Property.Color
 }
